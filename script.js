74f6f53d6d3055d4db710e296ec0549e173060e4
esteranodin <esteranodin@hotmail.fr>
2024-11-25 17:01:43 +0100

    ADD : comments

--- script.js
+++ script.js
@@ -1,33 +1,37 @@
-
+document.addEventListener('keyup', handleStopAudioAndAnimation);
 document.addEventListener('keydown', handleDrumPlay)
 
 function handleDrumPlay(event) {
+    // pour ne pas que le son se répète
+    if (event.repeat) {
+        return;
+    }
 
-    // variable qui stock la selection de la source via data-key et lui indique le bon keyCode a mettre entre "" dans la source 
+    let keyWhich = document.querySelector(`.key[data-key="${event.keyCode}"]`);
+
+    if (!keyWhich) {
+        return;
+    };
+
+    // variable qui stock la selection de la source via data-key et lui indique le bon keyCode a mettre entre "" dans la source
     let keyWhichAudio = document.querySelector(
         'audio[data-key="' + event.keyCode + '"]');
+
+    if (keyWhichAudio) {
+        // pour lancer l'audio
+        keyWhichAudio.play();
+    }
+
     // guillements insérées en texte par ' ' + réécris chemin pour selecteurs css audio
     // avec $ pour éviter concaténation let audio = document.querySelector( `audio[data-key="${event.keyCode}"]`)   
-   
-    let keyWhich = document.querySelector(`.key[data-key="${event.keyCode}"]`);
-    
-    if (event.repeat){
-        return;
-    }
 
-    if (!keyWhichAudio) {
-        return;
-    }
-    
-  // pour lancer l'audio
-    keyWhichAudio.play();
 
-    keyWhich.classList.toggle('playing');   
+    keyWhich.classList.toggle('playing');
+
 };
 
-document.addEventListener('keyup', handleStopAudioAndAnimation)
 
-function handleStopAudioAndAnimation (event) {
+function handleStopAudioAndAnimation(event) {
     let audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
     let keyWhich = document.querySelector(`.key[data-key="${event.keyCode}"]`);
 
@@ -35,14 +39,9 @@ function handleStopAudioAndAnimation (event) {
     //     return;
     // }
 
-keyWhich.classList.remove('playing');
+    keyWhich.classList.remove('playing');
 };
 
 
-const keyRecord = document.querySelector(`.key[data-key="32"]`);
 
-keyRecord.addEventListener ('keydown', handleRecord);
-function handleRecord (event) {
 
-    console.log('yayaayay');
-};
