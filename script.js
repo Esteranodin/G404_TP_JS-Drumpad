b751e80d14fef8c98a11378641b8f50168fa5efd
esteranodin <esteranodin@hotmail.fr>
2024-11-25 16:44:13 +0100

    ADD : pad sons et animations

--- script.js
+++ script.js
@@ -1,13 +1,48 @@
-const root = document.querySelector(':root');
-const keys = document.querySelector('.pad');
-const audio = document.querySelectorAll('audio');
 
-root.addEventListener('keydown', handleDrumPlay)
-console.log(audio);
+document.addEventListener('keydown', handleDrumPlay)
 
 function handleDrumPlay(event) {
-const keyWhich = event.keyCode;
-audio.includes(keyWhich);
+
+    // variable qui stock la selection de la source via data-key et lui indique le bon keyCode a mettre entre "" dans la source 
+    let keyWhichAudio = document.querySelector(
+        'audio[data-key="' + event.keyCode + '"]');
+    // guillements insérées en texte par ' ' + réécris chemin pour selecteurs css audio
+    // avec $ pour éviter concaténation let audio = document.querySelector( `audio[data-key="${event.keyCode}"]`)   
+   
+    let keyWhich = document.querySelector(`.key[data-key="${event.keyCode}"]`);
+    
+    if (event.repeat){
+        return;
+    }
+
+    if (!keyWhichAudio) {
+        return;
+    }
+    
+  // pour lancer l'audio
+    keyWhichAudio.play();
+
+    keyWhich.classList.toggle('playing');   
 };
 
-// root.classList.toggle(".playing");
+document.addEventListener('keyup', handleStopAudioAndAnimation)
+
+function handleStopAudioAndAnimation (event) {
+    let audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+    let keyWhich = document.querySelector(`.key[data-key="${event.keyCode}"]`);
+
+    // if (!keyWhichAudio) {
+    //     return;
+    // }
+
+keyWhich.classList.remove('playing');
+};
+
+
+const keyRecord = document.querySelector(`.key[data-key="32"]`);
+
+keyRecord.addEventListener ('keydown', handleRecord);
+function handleRecord (event) {
+
+    console.log('yayaayay');
+};
