059a58f4bcb6bacf18ff2021cd725718a9f916e4
esteranodin <esteranodin@hotmail.fr>
2024-11-26 12:36:06 +0100

    ADD : boutons reccord et play

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-document.addEventListener('keydown', handleDrumPlay)
+document.addEventListener('keydown', handleDrumPlay);
 document.addEventListener('keyup', handleStopAudioAndAnimation);
 
 function handleDrumPlay(event) {
-    // pour ne pas que le son se répète et placer au début pour eviter tout autre lancement de code
+    // pour ne pas que le son se répète placé au début pour ne pas lancer du code pour rien
     if (event.repeat) {
         return;
     }
 
     // variable qui stock la selection de la source via data-key et lui indique le bon keyCode a mettre entre "" dans la source
-    let keyWhich = document.querySelector(`.key[data-key="${event.keyCode}"]`);
+    const keyWhich = document.querySelector(`.key[data-key="${event.keyCode}"]`);
 
     if (!keyWhich) {
         return;
     };
-    
+
     keyWhich.classList.toggle('playing');
 
     // pareil que première variable + guillemets pour insérées en texte par ' ' + réécris "chemin" pour selecteurs css audio   
@@ -21,26 +21,44 @@ function handleDrumPlay(event) {
     // avec $ pour éviter concaténation let audio = document.querySelector( `audio[data-key="${event.keyCode}"]`)  
 
     if (!keyWhichAudio) {
+        //identificateur pour record
+        // if(){
+        //     triggerRecord();
+        // };
+
+        //identificateur pour play 
+        // if(){
+        //     triggerPlay();
+        // }
+
         return;
     };
 
     if (keyWhichAudio) {
+        // pour remettre le son au début et pouvoir l'enchainer autant de fois qu'on appuye
+        keyWhichAudio.currentTime = 0;
         // pour lancer l'audio
         keyWhichAudio.play();
     };
 };
 
-
 function handleStopAudioAndAnimation(event) {
-    let audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
-    let keyWhich = document.querySelector(`.key[data-key="${event.keyCode}"]`);
-   
-    //possible d'abreger si un seul ordre
-    if (!keyWhich) return;
+    let keySelect = document.querySelector(`.key[data-key="${event.keyCode}"]`);
 
-    keyWhich.classList.remove('playing');
+    //possible d'abreger si un seul ordre comme ci-dessous
+    if (!keySelect) return;
+
+    let keyWhichAudio = document.querySelector('audio[data-key="' + event.keyCode + '"]');
+
+    if(keySelect && keyWhichAudio){
+
+        keySelect.classList.toggle('playing');
+    }
+
 };
 
+function triggerRecord(){
 
+}
 
 
