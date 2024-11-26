f7de4b94a31e8154a18e99bbd76a3f88b3dd2117
esteranodin <esteranodin@hotmail.fr>
2024-11-26 16:53:10 +0100

    ADD : enregistrement

--- script.js
+++ script.js
@@ -1,62 +1,75 @@
 document.addEventListener('keydown', handleDrumPlay);
 document.addEventListener('keyup', handleStopAudioAndAnimation);
 
+let recording = false;
+let soundsRecording = [];
+let times = [];
+
 function handleDrumPlay(event) {
-    
-    if (event.repeat) { // pour ne pas que le son se répète placé au début pour ne pas lancer du code pour rien une fois la fonction faite une 1ere fois
-        return;
-    }
+    if (event.repeat) return;
 
-    const keyWhich = document.querySelector(`.key[data-key="${event.keyCode}"]`); // variable qui stock la selection de la source via data-key et lui indique le bon keyCode a mettre entre "" dans la source
+    const keyWhich = document.querySelector(`.key[data-key="${event.keyCode}"]`);
 
-    if (!keyWhich) { // si autre touche n'ayant pas d'écouteur d'évènement alors ne lance pas le reste du code
-        return;
-    };
+    if (!keyWhich) return;
 
-    keyWhich.classList.toggle('playing'); //ajoute classe CSS contenant animation
-      
-    const keyWhichAudio = document.querySelector('audio[data-key="' + event.keyCode + '"]'); // pareil que première variable + guillemets pour insérées en texte par ' ' + réécris "chemin" pour selecteurs css audio
-    // avec $ pour éviter concaténation let KeyWhichAudio = document.querySelector( `audio[data-key="${event.keyCode}"]`)  
+    keyWhich.classList.toggle('playing');
 
-    if (!keyWhichAudio) { // si autre touche n'ayant pas d'écouteur d'évènement et pas de source audio alors ne lance pas le reste du code
+    const keyWhichAudio = document.querySelector('audio[data-key="' + event.keyCode + '"]');
 
-        if(event.keyCode === 82){ // si pas audio et si touche R
-            handleTriggerRecord();
+    if (!keyWhichAudio) {
+
+        if (event.keyCode === 82) {
+            triggerRecord(event);
         };
 
-        if(event.keyCode === 32){ // si pas audio et si touche espace
-            handleTriggerPlay();
-        }
+        
+        // if(event.keyCode === 32){ 
+        //     triggerPlay();
+        // }
 
         return;
     };
 
     if (keyWhichAudio) {
-        
-        keyWhichAudio.currentTime = 0; // pour remettre le son au début et pouvoir l'enchainer autant de fois qu'on appuye
-        
-        keyWhichAudio.play(); // pour lancer l'audio
+
+        keyWhichAudio.currentTime = 0;
+        keyWhichAudio.play();
+
+        if (recording === true) {
+            record(event);
+        };
     };
 };
 
 function handleStopAudioAndAnimation(event) {
     let keySelect = document.querySelector(`.key[data-key="${event.keyCode}"]`);
 
-    if (!keySelect) return; //possible d'abreger si un seul ordre comme ci-dessous
+    if (!keySelect) return;
 
     let keyWhichAudio = document.querySelector('audio[data-key="' + event.keyCode + '"]');
 
-    if(keySelect && keyWhichAudio){ //pour isoler reccord et play et qu'animation reste après keyUp
+    if (keySelect && keyWhichAudio) {
         keySelect.classList.toggle('playing');
     }
 };
 
-function handleTriggerRecord(){
-console.log("hehrjhez");
-
+function triggerRecord(event) {
+    if (event.keyCode === 82 && recording === false) {
+        recording = true;
+    }
+    else {
+        recording = false;
+    };
 };
 
-// function handleTriggerPlay(){
+function record(event) {
+            soundsRecording.push(event.keyCode);
+            console.log(soundsRecording);
+            
+};
+
+
+// function triggerPlay(){
 
 // };
 
--- script_commented.js
+++ script_commented.js
@@ -1,8 +1,12 @@
 document.addEventListener('keydown', handleDrumPlay);
 document.addEventListener('keyup', handleStopAudioAndAnimation);
 
+let recording = false;
+let soundsRecording = [];
+let times = [];
+
 function handleDrumPlay(event) {
-    
+
     if (event.repeat) { // pour ne pas que le son se répète placé au début pour ne pas lancer du code pour rien une fois la fonction faite une 1ere fois
         return;
     }
@@ -14,28 +18,32 @@ function handleDrumPlay(event) {
     };
 
     keyWhich.classList.toggle('playing'); //ajoute classe CSS contenant animation
-      
+
     const keyWhichAudio = document.querySelector('audio[data-key="' + event.keyCode + '"]'); // pareil que première variable + guillemets pour insérées en texte par ' ' + réécris "chemin" pour selecteurs css audio
     // avec $ pour éviter concaténation let KeyWhichAudio = document.querySelector( `audio[data-key="${event.keyCode}"]`)  
 
     if (!keyWhichAudio) { // si autre touche n'ayant pas d'écouteur d'évènement et pas de source audio alors ne lance pas le reste du code
 
-        if(event.keyCode === 82){ // si pas audio et si touche R
-            handleTriggerRecord();
+        if (event.keyCode === 82) { // si pas audio et si touche R
+            triggerRecord();
         };
 
-        if(event.keyCode === 32){ // si pas audio et si touche espace
-            handleTriggerPlay();
+        if (event.keyCode === 32) { // si pas audio et si touche espace
+            triggerPlay();
         }
 
         return;
     };
 
     if (keyWhichAudio) {
-        
+
         keyWhichAudio.currentTime = 0; // pour remettre le son au début et pouvoir l'enchainer autant de fois qu'on appuye
-        
+
         keyWhichAudio.play(); // pour lancer l'audio
+
+        if (recording === true) { // si r est enfoncé on lance la focntion enregistrement ici pour qu eça ne prenne en compte que les touches concernées par le pad
+            record(event);
+        };
     };
 };
 
@@ -46,18 +54,28 @@ function handleStopAudioAndAnimation(event) {
 
     let keyWhichAudio = document.querySelector('audio[data-key="' + event.keyCode + '"]');
 
-    if(keySelect && keyWhichAudio){ //pour isoler reccord et play et qu'animation reste après keyUp
+    if (keySelect && keyWhichAudio) { //pour isoler reccord et play et qu'animation reste après keyUp
         keySelect.classList.toggle('playing');
     }
 };
 
-function handleTriggerRecord(){
-console.log("hehrjhez");
+function triggerRecord(event) { //fonction pour enregistrer
+
+    if (event.keyCode === 82 && Recording === false) { // si evenement declenche div 82 et si ma variable est égale a sa valeur initiale 
+        Recording = true; // alors la variable prend la valeur true
+    }
+
+    else {
+        Recording = false; // ou alors la variable prend la valeur false
+    };
 
 };
 
-// function handleTriggerPlay(){
+function record(event) { // fonctionne qui enregistre en poussant dans un tableau
+    soundsRecording.push(event.keyCode);
+};
 
-// };
+function triggerPlay() {
+};
 
 
